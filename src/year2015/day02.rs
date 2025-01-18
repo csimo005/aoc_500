fe31be1b3b0e7985cc1a91d150d9a33360880311
Cody Simons <csimo005@ucr.edu>
2025-01-17 23:48:55 -0800

    Initial solution for y2015d02, unoptimized

--- src/year2015/day02.rs
+++ src/year2015/day02.rs
@@ -1,11 +1,33 @@
-pub fn parse(_input: &str) -> Vec<i32> {
-    todo!()
+use std::cmp;
+
+pub fn parse(input: &str) -> Vec<i32> {
+    let mut data = Vec::<i32>::new();
+    for line in input.lines() {
+        for f in line.split("x") {
+            data.push(f.parse::<i32>().unwrap());
+        }
+    }
+
+    data
 }
 
-pub fn part1(_input: &[i32]) -> i32 {
-    todo!()
+pub fn part1(input: &[i32]) -> i32 {
+    let mut total: i32 = 0;
+    for i in (0..input.len()).step_by(3) {
+        let (l, w, h) = (input[i], input[i+1], input[i+2]);
+        total += 2 * (l * w + w * h + l * h) + (l * w * h) / cmp::max(cmp::max(l, w), h);
+    }
+
+    total
 }
 
-pub fn part2(_input: &[i32]) -> usize {
-    todo!()
+pub fn part2(input: &[i32]) -> i32 {
+    let mut total: i32 = 0;
+    for i in (0..input.len()).step_by(3) {
+        let (l, w, h) = (input[i], input[i+1], input[i+2]);
+        total += 2 * (l + w + h - cmp::max(cmp::max(l, w), h));
+        total += l * w * h;
+    }
+
+    total
 }
